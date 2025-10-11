e072bc36a351eaaf5977f2aba465db65144771de
blvckmamba-glitch <Osinachi.Justin-Sonde@pau.edu.ng>
2025-10-11 13:57:25 +0100

    (week 2 project 1)

--- week-2/project_1.rs
+++ week-2/project_1.rs
@@ -1,7 +1,7 @@
 fn  main() {
 let principal:f64 = 520000000.00;  // initial amount
  let rate: f64 = 0.10;        // 10% annual interest
- let times: f64 = 4.0;
+ let times: f64 = 1.0;
     let years: f64 = 5.0;       // for 5 years
 
      let amount = principal * (1.0 + rate / times).powf(times * years);
